d4a7b4ab590e080c4463686583dcfe6ab77d6afe
qudalong <862792055@qq.com>
2019-01-21 17:06:13 +0800

    去除node_modules

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app"></div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text:{
+        type:String,
+        value:'123'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
